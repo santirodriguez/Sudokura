ba923cf577f5de31480f970e7187071716acc553
Santiago Rodriguez <15202529+santirodriguez@users.noreply.github.com>
2026-09-21 14:57:13 -0700

    Fix v1.3.5 validation expectations [ui-review]

--- tests/test_main.c
+++ tests/test_main.c
@@ -388,7 +388,7 @@ static void test_window_size_normalization(void) {
 static void test_i18n(void) {
   for (int language = 0; language < LANG_COUNT; ++language)
     for (int key = 0; key < T_COUNT; ++key) assert(tr((Language)language, (TextKey)key)[0]);
-  assert(!strcmp(SUDOKURA_VERSION, "1.3.0"));
+  assert(!strcmp(SUDOKURA_VERSION, "1.3.5"));
 }
 
 int main(void) {
